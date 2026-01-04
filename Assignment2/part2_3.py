d96312f54d81d548f16953b59f9901e4b479e5b9
Leon Orou <leon_orou@hotmail.com>
2026-01-04 17:51:02 +0100

    time/date correlation

--- Assignment2/part2_3.py
+++ Assignment2/part2_3.py
@@ -60,8 +60,8 @@ data['Minutes Since Measure Start'] = (data['Unnamed: 0'] - min_time).dt.total_s
 data = data.drop(columns=['Unnamed: 0'])
 corr = data.corr()  # dont include date column
 fig, ax = plt.subplots(figsize=(12, 10))
-cax = ax.matshow(corr, cmap='coolwarm')
-fig.colorbar(cax, label='Pearson Correlation Coefficient (-1 to 1)')
+cax = ax.matshow(corr, cmap='coolwarm', vmin=-1, vmax=1)
+fig.colorbar(cax, label='Pearson Correlation Coefficient (-1 to 1)', )
 
 ticks = np.arange(0, len(corr.columns), 1)
 ax.set_xticks(ticks)
@@ -73,3 +73,23 @@ plt.title('Correlation Matrix of Weather Data', pad=20)
 plt.tight_layout()
 plt.savefig('correlation_matrix.png')
 plt.show()
+
+corr_pairs = corr.unstack()  # Convert matrix to series
+corr_pairs = corr_pairs[corr_pairs != 1.0]  # Remove self-correlations
+corr_pairs = corr_pairs.drop_duplicates()  # Remove duplicate pairs
+corr_pairs = corr_pairs.sort_values(ascending=False, key=abs)  # Sort by absolute value
+
+print("\nTop 10 Highest Correlations:")
+print("-" * 70)
+for (var1, var2), corr_value in corr_pairs.head(10).items():
+    print(f"{var1:40s} <-> {var2:40s}: {corr_value:6.3f}")
+# Relative Humidity in %                   <-> Global Radiation in W/m2                : -0.545
+# Temperature Air in °C                    <-> Global Radiation in W/m2                :  0.505
+# Temperature Air in °C                    <-> Relative Humidity in %                  : -0.439
+# Relative Humidity in %                   <-> Minutes Since Measure Start             :  0.416
+# Windspeed in m/s                         <-> Wind Direction in Degrees               :  0.362
+# Precipitation in mm                      <-> Precipitation Duration in min           :  0.344
+# Relative Humidity in %                   <-> Precipitation Duration in min           :  0.280
+# Windspeed in m/s                         <-> Minutes Since Measure Start             : -0.244
+# Relative Humidity in %                   <-> Windspeed in m/s                        : -0.225
+# Precipitation Duration in min            <-> Global Radiation in W/m2                : -0.138
